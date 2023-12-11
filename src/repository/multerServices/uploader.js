a49ea56b855b5ffac1db48d811261d0f119010a9
Trinidad Gonzalez <mtgprimaria@gmail.com>
2023-12-11 17:53:51 -0300

    modifico ruta multer

--- src/repository/multerServices/uploader.js
+++ src/repository/multerServices/uploader.js
@@ -6,8 +6,10 @@ import path from 'path'
 function createUploader(destinationFolder) {
     const storage = multer.diskStorage({
         destination: function (req, file, callback) {
-            const destinationPath = path.join(`${__dirname}/public/files/${destinationFolder}`)
-            callback(null, destinationPath);
+           // const destinationPath = path.join(`${__dirname}/public/files/${destinationFolder}`)
+           const destinationPath = `/opt/render/project/src/src/public/files/${destinationFolder}`;
+           
+           callback(null, destinationPath);
         },
         filename: function (req, file, callback) {
             callback(null, `${Date.now()}-${file.originalname}`)
