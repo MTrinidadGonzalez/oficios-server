416795be3477e0b18096e3f5ce3e8ee64226445b
Trinidad Gonzalez <mtgprimaria@gmail.com>
2023-12-10 21:57:26 -0300

    .

--- src/routers/documents.router.js
+++ src/routers/documents.router.js
@@ -11,7 +11,7 @@ export default class DocumentsRouter extends RouterPadre{
             //urlCompleta: http://localhost:8081/api/documents/1694297444243-59799a2e8ccd217019f2cc9d87f42af7.jpg?folder=products
            
              const {filename}= req.params
-             const { folder } = req.query || 'img'
+             const { folder } = req.query || 'profile'
              const pathIMG= path.resolve(`${__dirname}/public/files/${folder}/${filename}`)
              const exist=  fs.existsSync(pathIMG)
              if(exist){
