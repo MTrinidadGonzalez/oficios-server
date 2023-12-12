ddf0003efc18a64d5888899e9b4bcbb3bec39864
Trinidad Gonzalez <mtgprimaria@gmail.com>
2023-12-11 23:31:38 -0300

    ..

--- src/routers/documents.router.js
+++ src/routers/documents.router.js
@@ -8,18 +8,20 @@ const router= Router()
 router.get('/:filename', async (req, res)=> {
     try {
         // urlCompleta: http://localhost:8081/api/documents/1694297444243-59799a2e8ccd217019f2cc9d87f42af7.jpg?folder=products
-
+        console.log('file router llega la peticion')
         const { filename } = req.params;
         const { folder } = req.query || 'img';
         // Modifica la construcción de la ruta para Render.com
         const pathIMG = `/opt/render/project/src/src/public/files/${folder}/${filename}`;
+        console.log('file router pathIMG',pathIMG)
         const exist = fs.existsSync(pathIMG);
-        
+        console.log('file router exist',exist)
+
         if (exist) {
-            console.log('existe el archivo y fue encontrado')
+            console.log('file router existe el archivo y fue encontrado')
             res.sendFile(pathIMG);
         } else {
-            console.log('no existe el archivo y no fue encontrado')
+            console.log('file router  no existe el archivo y no fue encontrado')
             res.send({ status: 'error', error: 'Documento no encontrado en el servidor' });
         }
     } catch (error) {
